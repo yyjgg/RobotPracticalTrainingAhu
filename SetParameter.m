64fd6f0257c9a5cf3826cf59caf87d7304b053b2
yyj <1176013133@qq.com>
2025-11-13 20:18:33 +0800

    小电脑 - Update commit message

---
+++ SetParameter.m
@@ -0,0 +1,21 @@
+%parameter file
+%下正，上负
+clc; clear;
+PowerUnit_CR = 1148;%
+PowerUnit_wb = -141.4;%
+PowerUnit_Tm = 0.02;%
+ConEfficiency_drone_R = 0.225;%
+ConEfficiency_drone_cT = 1.105e-05;%
+ConEfficiency_drone_cM = 1.779e-07;%
+PosDyna_Mass = 1.4;%
+AttDyna_Ixx = 0.0211;%
+AttDyna_Iyy = 0.0219;%
+AttDyna_Izz = 0.0366;%
+PosDyna_GravityAcc = 9.8;%
+POSITION_Z_ZERO = -100;%
+PWM_1 = 0;%0-1000
+PWM_2 = 0;
+PWM_3 = 0;
+PWM_4 = 0;
+PWM_ALL = 0;
+
--- main.m
+++
@@ -1,2 +0,0 @@
-%main file
-clc; clear;
